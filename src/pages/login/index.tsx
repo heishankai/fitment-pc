85fc17a74b54e64367e13db62ebcb0802297005b
kli59 <kli59@mail.yst.com.cn>
2025-10-02 19:09:01 +0800

    feat: 提交

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -58,7 +58,7 @@ const Login: React.FC = () => {
   return (
     <LoginContainer>
       <StyledLoginFormPage
-        backgroundImageUrl="https://mdn.alipayobjects.com/huamei_gcee1x/afts/img/A*y0ZTS6WLwvgAAAAAAAAAAAAADml6AQ/fmt.webp"
+        // backgroundImageUrl="https://mdn.alipayobjects.com/huamei_gcee1x/afts/img/A*y0ZTS6WLwvgAAAAAAAAAAAAADml6AQ/fmt.webp"
         backgroundVideoUrl="https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr"
         title="叮当智装"
         onFinish={handleConfirmSubmit}
